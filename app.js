04daecc2e9337a75e5901376289358dada1bfc6e
a00829642 <aleguamen_@hotmail.com>
2021-07-01 15:51:10 -0500

    mercado libre5

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(express.static('public'));
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath))
 
 
 app.listen(process.env.PORT || 3000, function() {
@@ -8,14 +13,16 @@ app.listen(process.env.PORT || 3000, function() {
 });
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.resolve(__dirname + './views/home.html'));
 });
 
-app.get('/register.html', (req, res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get('/register', (req, res)=>{
+    res.sendFile(path.resolve(__dirname + './views/register.html'));
 });
 
-app.get('/login.html', (req, res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+app.get('/login', (req, res)=>{
+    res.sendFile(path.resolve(__dirname + './views/login.html'));
 });
+
+
 
