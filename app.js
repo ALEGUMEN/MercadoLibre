8ad826caf285179580599a5385351df2c9f3626d
a00829642 <aleguamen_@hotmail.com>
2021-07-01 16:06:35 -0500

    mercadolibre7

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const path = require('path');
+let express = require('express');
+const { Http2ServerRequest } = require('http2');
+const  app = express();
+const path = require("path");   
 
-const app = express();
+const publicPath = path.resolve(__dirname, './public');  
+app.use(express.static(publicPath)); 
 
-const publicPath = path.resolve(__dirname, './public');
-
-app.use(express.static(publicPath))
 
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor funcionando en el puerto 3000');
 });
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/home.html'));
-});
 
-app.get('/register', (req, res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/register.html'));
-});
+app.get("/", function(req,res){
+    let htmlArchivo = path.resolve(__dirname, './views/home.html')
+    res.sendFile(htmlArchivo);
+})
 
-app.get('/login', (req, res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/login.html'));
-});
+app.get("/register", function(req,res){
+    let htmlArchivo = path.resolve(__dirname, './views/register.html')
+    res.sendFile(htmlArchivo);
+})
 
-
+app.get("/login", function(req,res){
+    let htmlArchivo = path.resolve(__dirname, './views/login.html')
+    res.sendFile(htmlArchivo);
+})
 
