c08b6246b385456cc414beb2ea43b348c2938117
a00829642 <aleguamen_@hotmail.com>
2021-07-01 15:39:32 -0500

    mercado libre4

--- app.js
+++ app.js
@@ -19,30 +19,3 @@ app.get('/login.html', (req, res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get("/public/images/logo-mercado-liebre.svg", (req, res)=>{
-    res.sendFile(__dirname + "/public/images/logo-mercado-liebre.svg");
-});
-
-app.get("/public/images/img-cafetera-moulinex.jpg", (req, res)=>{
-    res.sendFile(__dirname + "/public/images/img-cafetera-moulinex.jpg");
-});
-
-app.get("/public/images/img-home-banner.jpg", (req, res)=>{
-    res.sendFile(__dirname + "/public/images/img-home-banner.jpg");
-});
-
-app.get("/public/images/img-macbook-pro-2019.jpg", (req, res)=>{
-    res.sendFile(__dirname + "/public/images/img-macbook-pro-2019.jpg");
-});
-
-app.get("/public/images/img-samsung-galaxy-s10.jpg", (req, res)=>{
-    res.sendFile(__dirname + "/public/images/img-samsung-galaxy-s10.jpg");
-});
-
-app.get("/public/images/img-tv-samsung-smart.jpg", (req, res)=>{
-    res.sendFile(__dirname + "/public/images/img-tv-samsung-smart.jpg");
-});
-
-app.get("/public/images/img-tv-samsung-smart.jpg", (res, req)=>{
-    res.sendFile(__dirname + "/public/images/img-tv-samsung-smart.jpg");
-});
